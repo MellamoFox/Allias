40218915fc627622065feacc8d03f9c36e8ad126
MellamoFox <104905904+MellamoFox@users.noreply.github.com>
2022-11-12 17:20:54 +0200

    added sound model

--- Millioneros/Models/SoundModel.swift
+++ Millioneros/Models/SoundModel.swift
@@ -5,4 +5,13 @@
 //  Created by VT on 12.11.2022.
 //
 
-import Foundation
+import UIKit
+import AVFoundation
+
+var player: AVAudioPlayer!
+
+func playSound(resource: String) {
+    let url = Bundle.main.url(forResource: resource, withExtension: "mp3")
+    player = try! AVAudioPlayer(contentsOf: url!)
+    player?.play()
+}
